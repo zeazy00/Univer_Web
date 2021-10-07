79a565754fd009232e045cb7d3b8e6d1e3bb1f4c
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2021-10-07 15:58:36 +1000

    add: first screen markup

--- webapp/src/pages/first-screen/FirstScreen.tsx
+++ webapp/src/pages/first-screen/FirstScreen.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import './first-screen.scss';
+import SocialMediaList from "../../components/social-media-list/SocialMediaList";
 
 export default function FirstScreen(): JSX.Element {
     return (
         <section className="first-screen">
-
+            <section className="first-screen__logo">
+                весна всегда
+            </section>
+            <section className="first-screen__welcome">
+                флористика с душой
+            </section>
+            <SocialMediaList className='first-screen__social-medias_horizontal'/>
+            <div className="first-screen__gradient"></div>
         </section>
     )
 }
